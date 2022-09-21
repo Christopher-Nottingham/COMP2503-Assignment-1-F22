0b42c054e7056e55dbbc468ab58491fffd35d434
Christopher-Nottingham <cnott729@mtroyal.ca>
2022-09-20 18:07:56 -0600

    Creating instance of database

--- src/application/AppDriver.java
+++ src/application/AppDriver.java
@@ -1,10 +1,12 @@
 package application;
 
+import controller.DatabaseManager;
+
 public class AppDriver {
-
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
-
+    DatabaseManager db = new DatabaseManager();
+    db.loabDB();
   }
-
 }
+
+
